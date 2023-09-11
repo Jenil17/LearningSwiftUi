630aa0d1420d72a689abea2408a1c511ae527823
Jenil17 <jeniljariwala64@gmail.com>
2023-09-11 10:28:40 -0400

    Initial Commit

---
+++ IntroToSwfitUi/IntroToSwfitUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroToSwfitUiApp.swift
+//  IntroToSwfitUi
+//
+//  Created by Jenil Jariwala on 2023-09-11.
+//
+
+import SwiftUI
+
+@main
+struct IntroToSwfitUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
